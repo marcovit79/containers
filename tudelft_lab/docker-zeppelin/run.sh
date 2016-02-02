7bcd333891260dfb66d811422f71042c329986ec
Marco Vit <mvit1@tudelft.nl>
2016-02-02 08:53:13 +0100

    Start zeppenin

--- tudelft_lab/docker-zeppelin/run.sh
+++ tudelft_lab/docker-zeppelin/run.sh
@@ -1,1 +1,8 @@
-/zeppelin_code/zeppelin-daemon.sh start
+#!/bin/bash
+trap ' /home/zeppelin/zeppelin_installation/bin/zeppelin-daemon.sh stop ; exit' TERM
+
+
+zeppelin_installation/bin/zeppelin-daemon.sh start
+
+# keep live the container - bash check signals only between sleep
+while :; do sleep 5; echo "running"; done  
