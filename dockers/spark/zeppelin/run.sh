090ea0bb7917af46f579848247b17f4ee181c54c
Marco Vit <mvit1@tudelft.nl>
2016-02-07 12:24:54 +0100

    Mount spark filesystem

--- dockers/spark/zeppelin/run.sh
+++ dockers/spark/zeppelin/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 trap '/home/zeppelin/zeppelin_installation/bin/zeppelin-daemon.sh stop ; exit' TERM
 
+echo "Mount spark_fs"
+sudo mount /mnt/spark_fs/
 
+echo "Start zeppelin"
 zeppelin_installation/bin/zeppelin-daemon.sh start
 
 # keep live the container - bash check signals only between sleep
