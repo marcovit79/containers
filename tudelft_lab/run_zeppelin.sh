d1f02f0010b5870efb0645d7d05ca2c1473c4d6b
Marco Vit <mvit1@tudelft.nl>
2016-02-08 09:32:01 +0100

    Use same VM gluster rserver

--- tudelft_lab/run_zeppelin.sh
+++ tudelft_lab/run_zeppelin.sh
@@ -4,7 +4,7 @@ vagrant ssh vm2 -c "docker stop zeppelin1 ; docker rm zeppelin1"
 
 vagrant ssh vm2 -c "\$( ./weave env ) && \
                     docker run -d \
-                          --add-host gluster-seed-server:\$( ./weave dns-lookup glusterserver1 ) \
+                          --add-host gluster-seed-server:\$( ./weave dns-lookup glusterserver2 ) \
                           --privileged \
                           --name zeppelin1 \
                           mvit79/spark-zeppelin"
