5d2cde947f5e2dfd40c1357da59ac7e2568a8e5f
Marco Vit <mvit1@tudelft.nl>
2016-02-11 09:13:51 +0100

    Try dumb-init

--- tudelft_lab/run_spark.sh
+++ tudelft_lab/run_spark.sh
@@ -1,11 +1,16 @@
 vagrant ssh vm1 -c "docker pull mvit79/spark-node"
 
 vagrant ssh vm1 -c "docker stop spark-master ; docker rm spark-master"
-
 vagrant ssh vm1 -c "\$( ./weave env ) && \
                     docker run \
                            --add-host gluster-seed-server:\$( ./weave dns-lookup glusterserver1 ) \
                            --privileged \
-                           --name spark-master -d mvit79/spark-node \
-                           spark-master.weave.local"
+                           --name spark-master -d mvit79/spark-node "
 
+vagrant ssh vm1 -c "docker stop spark-worker1 ; docker rm spark-worker1"
+vagrant ssh vm1 -c "\$( ./weave env ) && \
+                    docker run \
+                           --add-host gluster-seed-server:\$( ./weave dns-lookup glusterserver1 ) \
+                           --privileged \
+                           --name spark-worker1 -d mvit79/spark-node \
+                           spark-master.weave.local"
