3133282b36888db54ae5803608ea714fed94aeb5
Marco Vit <mvit1@tudelft.nl>
2016-02-11 01:28:36 +0100

    start openvpn on debian

--- weave/launch.sh
+++ weave/launch.sh
@@ -1,3 +1,8 @@
+vagrant ssh vm1  -c "sudo /vagrant/runvpn_from_machine.sh 192.168.33.11"
+vagrant ssh vm1  -c "sudo systemctl restart openvpn@tcp443"
+vagrant ssh vm1  -c "cp /etc/openvpn/client.ovpn /vagrant"
+
+
 vagrant ssh vm1  -c "./weave launch --init-peer-count 2"
 vagrant ssh vm1  -c "./scope launch"
 
@@ -8,7 +13,4 @@ vagrant ssh vm2  -c "./scope launch"
 vagrant ssh vm2  -c "./weave connect  192.168.33.11"
 vagrant ssh vm1  -c "./weave expose"
 
-vagrant ssh vm1  -c "sudo /vagrant/runvpn_from_machine.sh 192.168.33.11"
-vagrant ssh vm1  -c "sudo service openvpn restart"
-vagrant ssh vm1  -c "cp /etc/openvpn/client.ovpn /vagrant"
 
