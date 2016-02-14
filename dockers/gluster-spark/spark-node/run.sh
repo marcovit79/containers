3d7a8215ae068f51d9a6369d5bf8c11ba23ddcee
Marco Vit <mvit1@tudelft.nl>
2016-02-14 21:48:44 +0100

    WIP: refactoring to have all images in one repository to use less network resources.\n\nI also make run.sh more parametrized

--- dockers/gluster-spark/spark-node/run.sh
+++ dockers/gluster-spark/spark-node/run.sh
@@ -40,9 +40,15 @@ do
 done
 
 
-echo "Starting spark with master ${SPARK_MASTER_SERVER_NAME} "
 export SPARK_HOME="${HOME}/spark/"
+echo "SPARK_HOME = \"${SPARK_HOME}\""
 
+echo "Prepare GlusterFS-HDFS config"
+cat /etc/spark/core-site.xml.template | sed -e "s|SPARK_FS_PATH|${SPARK_FS_MOUNT_POINT}|" \
+                                      > ${SPARK_HOME}/conf/core-site.xml
+
+
+echo "Starting spark with master ${SPARK_MASTER_SERVER_NAME} "
 ${HOME}/spark/sbin/stop-slave.sh
 ${HOME}/spark/sbin/stop-master.sh
 
